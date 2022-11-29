a36b6714953dcd8f7b7bf594fa9ea0b814018faf
William Fenton <w.fenton22@gmail.com>
2022-11-29 09:08:59 -0500

    Rewrite playRound function

--- script.js
+++ script.js
@@ -9,28 +9,28 @@ function getComputerChoice() {
     } 
 } 
 
-function playRound(playerSelection, computerSelection) {
-   if(playerSelection === 'Rock' && computerSelection === 'Scissors')  {
-    return 'Rock DESTROYS Scissors, you are victorious!';
-   } else if(playerSelection === 'Rock' && computerSelection === 'Paper') {
-    return 'Paper covers Rock, you are defeated!';
-   } else if(playerSelection === 'Rock' && computerSelection === 'Rock') {
-    return 'It\'s a draw, get back in the ring!';
-   } else if(playerSelection === 'Paper' && computerSelection === 'Rock') {
-    return 'Paper covers Rock, you are victorious!';
-   }else if(playerSelection === 'Paper' && computerSelection === 'Scissors') {
-    return 'Scissors lacerates Paper, you are defeated!';
-   } else if(playerSelection === 'Paper' && computerSelection === 'Paper') {
-    return 'It\'s a draw, get back in the ring!';
-   } else if(playerSelection === 'Scissors' && computerSelection === 'Paper') {
-    return 'Scissors lacerates Paper, you are victorious!';
-   } else if(playerSelection === 'Scissors' && computerSelection === 'Rock') {
-    return 'Rock DESTROYS Scissors, you are defeated!';
-   } else if(playerSelection === 'Scissors' && computerSelection === 'Scissors') {
-    return 'Looks like the two of you are Scissoring, everybody wins! Giggity!';
-   } else {
-    return 'Something has went terribly wrong, try again!';
-   }
+
+
+function playRound(playerSelection,computerSelection ) {
+    if(playerSelection === 'Scissors' && computerSelection === 'Scissors') {
+        return 'Everybody wins when you\'re scissoring ;)';
+    } else if(playerSelection === computerSelection) {
+        return 'It\'s a tie, get back in the ring!';
+    } else if(playerSelection === 'Rock' && computerSelection === 'Scissors')  {
+        return 'Rock DESTROYS Scissors, you are victorious!';
+    } else if(playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return 'Paper covers Rock, you are victorious!';
+    } else if(playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return 'Scissors lacerates Paper, you are victorious!';
+    } else if(playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return 'Paper covers Rock, you are defeated!';
+    } else if(playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return 'Scissors lacerates Paper, you are defeated!';
+    } else if(playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'Rock DESTROYS Scissors, you are defeated!';
+    } else {
+        return 'Something has gone terribly wrong, try again!';
+    }
 }
 
 const playerSelection = prompt("Choose Rock, Paper, or Scissors!");
